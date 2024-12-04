8f77b06413a4369e471e8ce13b4a6b931f8e5211
Bberto-22 <158284893+Bberto-22@users.noreply.github.com>
2024-12-04 14:50:39 +0100

    Add files via upload

--- advent_of_code/day4/problem4.py
+++ advent_of_code/day4/problem4.py
@@ -1,23 +1,36 @@
 file_path = "input_problem4"
 
-def get_valid(matriz, i, j, letra):
-	for x in range(i - 1, i + 2):
-        	for y in range(j - 1, j + 2):
-        		if matriz[x][y] == letra:
-        			return x, y
-        		
+
+def search_word(matriz, i, j, palabra, direccion):
+    filas = len(matriz)
+    columnas = len(matriz[0])
+
+    for k, letra in enumerate(palabra):
+        x = i + k * direccion[0]
+        y = j + k * direccion[1]
+
+        if x < 0 or x >= filas or y < 0 or y >= columnas or matriz[x][y] != letra:
+            return False
+    return True
+
+
+def get_valid(matriz, i, j, palabra):
+    direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+    direcciones_correctas = 0
+    for direccion in direcciones:
+        if search_word(matriz, i, j, palabra, direccion):
+            direcciones_correctas += 1
+    return direcciones_correctas
+
 
 filas = 0
 columnas = 0
 with open(file_path, "r") as file:
-	for i, line in enumerate(file):
-		if i == 0:
-			columnas = len(line.strip())
-		filas += 1
-		
-print(filas)
-print(columnas)
-		
+    for i, line in enumerate(file):
+        if i == 0:
+            columnas = len(line.strip())
+        filas += 1
+
 matriz = []
 for _ in range(filas):
     fila = [None] * columnas
@@ -32,7 +45,6 @@ resultado = 0
 for i in range(len(matriz)):
     for j in range(len(matriz[i])):
         if matriz[i][j] == "X":
-        	x, y = get_valid(matriz, i, j, "M")
-        	x, y = get_valid(matriz, x, y, "A")
-        	if get_valid(matriz, i, j, "S"):
-        		resultado += 1
+            resultado += get_valid(matriz, i, j, "XMAS")
+
+print(resultado)
